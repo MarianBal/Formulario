e93ad067c5d25a75f53c19d804e9f8da05d02e3c
MarianBal <44822014+MarianBal@users.noreply.github.com>
2019-04-30 19:14:18 -0300

    M

--- formulario.js
+++ formulario.js
@@ -1,70 +1,47 @@
-console.log('Hola mundo')
+console.log('Hola mundo');
 
-var form = document.querySelector('form');
+const form = document.querySelector('form');
 
-var nombre = document.getElementById('name');
-var pers = document.getElementById('persona')
+const nombre = document.getElementById('name');
+const pers = document.getElementById('persona');
 
-var edad = document.getElementById('age');
-var preg = document.getElementById('pregunta');
+const edad = document.getElementById('age');
+const preg = document.getElementById('pregunta');
 
-var soltero = document.getElementById('single')
-var casado = document.getElementById('taken')
-var corazon = document.getElementById('corazon')
+const soltero = document.getElementById('single');
+const casado = document.getElementById('taken');
+const corazon = document.getElementById('corazon');
 
-var nac = document.getElementById('nationality')
-var bandera = document.getElementById('nac')
+const nac = document.getElementById('nationality');
+const bandera = document.getElementById('nac');
 
+const persona = () => pers.classList.add('naranja');
+const noPersona = () => pers.classList.remove('naranja');
 
-function persona(){
-    pers.classList.add('naranja')
-}
+const pregunta = () => preg.classList.add('naranja');
+const noPregunta = () => preg.classList.remove('naranja');
 
-function noPersona(){
-    pers.classList.remove('naranja')
-}
+const estado = () => corazon.classList.add('naranja');
+const noEstado = () => corazon.classList.remove('naranja');
 
-function pregunta(){
-    preg.classList.add('naranja')
-}
-
-function noPregunta(){
-    preg.classList.remove('naranja')
-};
-
-function estado(){
-    corazon.classList.add('naranja')
-}
-
-function noEstado(){
-    corazon.classList.remove('naranja')
-};
-
-function nacionalidad(){
-    bandera.classList.add('naranja')
-}
-
-function noNacionalidad(){
-    bandera.classList.remove('naranja')
-};
+const nacionalidad = () => bandera.classList.add('naranja');
+const noNacionalidad = () => bandera.classList.remove('naranja');
 
 
-
-
-form.onsubmit = function(e){
+form.onsubmit = (e) => {
 
     e.preventDefault();
 
     if(!nombre.value.length){
 
         nombre.classList.add('error')   
-        persona.classList.add('rojo') 
+        pers.classList.add('rojo') 
     }
 
-    if(edad.value< 18 || edad.value > 120){
+    if(edad.value< 18 || edad.value > 120 || edad.value ===""){
  
         edad.classList.add('error') 
-        pregunta.classList.add('rojo')
+        preg.classList.add('rojo')
     
     }
     if(!login.status[0].checked && !login.status[1].checked){
@@ -72,7 +49,7 @@ form.onsubmit = function(e){
         corazon.classList.add('rojo')
     }
 
-    var estadoCivil = false;
+    let estadoCivil = false;
 
     if(login.status[0].checked || login.status[1].checked){
 
@@ -86,7 +63,7 @@ form.onsubmit = function(e){
 
 }
 
-function agregarInvitado(){
+const agregarInvitado = () => {
 
     var lista = document.getElementById('invitados')
     var invitado = document.createElement('div')
@@ -104,27 +81,27 @@ function agregarInvitado(){
         estado = 'No me presenten a nadie'
     }
 
-    var i = nac.selectedIndex;
-    var pais = nac.options[i].value;
+    const i = nac.selectedIndex;
+    const pais = nac.options[i].value;
 
-    var spanes = ['Nombre: ', 'Edad: ', 'Estado Civil: ', 'Nacionalidad: '];
-    var datos = [nombre.value, edad.value, estado, pais]
+    const spanes = ['Nombre: ', 'Edad: ', 'Estado Civil: ', 'Nacionalidad: '];
+    const datos = [nombre.value, edad.value, estado, pais]
 
-    for(var i=0; i<spanes.length; i++){
-        var span = document.createElement('span');
+    for(let i=0; i<spanes.length; i++){
+        const span = document.createElement('span');
         span.textContent = spanes[i];
 
-        var input = document.createElement('input');
+        const input = document.createElement('input');
         input.value = datos[i];
 
-        var br = document.createElement('br');
+        const br = document.createElement('br');
 
         invitado.appendChild(span);
         invitado.appendChild(input);
         invitado.appendChild(br);
     }
 
-    var boton = document.createElement('button');
+    const boton = document.createElement('button');
     boton.textContent = 'borrar';
 
     invitado.appendChild(boton);
